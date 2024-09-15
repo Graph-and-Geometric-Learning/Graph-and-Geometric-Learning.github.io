df7f00b0d94e0a2f30432c5393d03c990f711599
Boltzmachine <qiuweikang1999@gmail.com>
2024-09-15 17:02:47 -0400

    add output

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/",
+    output: "export",
     images: {
         formats: ["image/avif", "image/webp"],
         remotePatterns: [
