d09f8739bb819ee4bd2fb887c1e81f49b01bb1b1
Boltzmachine <qiuweikang1999@gmail.com>
2026-07-07 19:20:29 -0700

    update weikang image

--- config/people.ts
+++ config/people.ts
@@ -47,7 +47,7 @@ export const peopleList = {
       intro:
         "My research focuses on the intersection of machine learning and neuroscience. The ultimate goal of my research is to transform humans - or at least himself - to AIs. This represents a pathway for humanity to transcend its biological limitations (e.g. immortality) and is the only way for modern humans to evolve. In addition to my academic pursuits, Weikang actively contributes to several open-source projects, such as Blender.",
       homepage: "https://github.com/Boltzmachine",
-      photo: "/people/weikang.png",
+      photo: "/people/weikang.jpeg",
     },
     {
       name: "Ngoc Bui",
